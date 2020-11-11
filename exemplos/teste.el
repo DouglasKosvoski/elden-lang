cc2c07c8c84a44dd51fc4f1263bec60826829d60
Douglas Kosvoski <douglas.contactpro@gmail.com>
2020-11-11 11:36:57 -0300

    Estrutura inicial do projeto

---
+++ exemplos/teste.el
@@ -0,0 +1,46 @@
+# declaracao de variavel
+summon a
+int a;
+
+# Atribuicao de valor a variavel
+a = 6
+cast a as 6
+cast a as one head falling down the road
+
+# matematica
+# equals
+== = matches
+0 = nothingness
+
+# sum
+a += 1
+a ascend 1
+a ascend by thyself
+
+# subtraction
+a -= 1
+a descend 1
+a descend by death
+
+# multiply
+a *= 7
+a strengthen 7
+a strengthen by the numbers of sinners in the room
+
+# division
+a /= 4
+a weaken 4
+a weaken by the forces of god
+
+# modulo
+a % 3
+a residual 3
+a residual from fire and ice
+
+# while
+while (a % 2 == 0)
+epoch a residual toxic waste matches nothingness begin
+    begin
+        asdasd
+    end
+end
