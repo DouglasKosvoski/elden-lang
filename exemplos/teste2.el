7e80cbb2565ea5fd9a8658a545177d14e13c5d79
Douglas Kosvoski <douglas.contactpro@gmail.com>
2020-11-25 12:59:42 -0300

    Inicializacao ta funcionando tambem

--- exemplos/teste2.el
+++ exemplos/teste2.el
@@ -1,4 +1,16 @@
-summon int Axabaqui.
+summon int Crack.
 summon double Creu.
+
 summon string Shhhh.
+
 summon boolean Nhon.
+
+
+cast Crack as candy.
+
+
+cast Creu as nonetheless.
+cast Shhhh as rollercoster.
+
+
+cast Nhon as true.
--- fonte/elden/main/Leitor.java
+++ fonte/elden/main/Leitor.java
@@ -14,6 +14,10 @@ public class Leitor {
   private String filename;
   private Scanner conteudo;
   ArrayList<String> conteudoAnalisado = new ArrayList<String>();
+  HashMap<String, TipoInteiro> vInt = new HashMap<String, TipoInteiro>();
+  HashMap<String, TipoDouble> vDouble = new HashMap<String, TipoDouble>();
+  HashMap<String, TipoString> vString = new HashMap<String, TipoString>();
+  HashMap<String, TipoBooleano> vBoolean = new HashMap<String, TipoBooleano>();
 
   // Constructors
   public Leitor(String filename) {
@@ -60,15 +64,15 @@ public class Leitor {
   }
 
   public void show() {
-    HashMap<String, TipoInteiro> vInt = new HashMap<String, TipoInteiro>();
-    HashMap<String, TipoDouble> vDouble = new HashMap<String, TipoDouble>();
-    HashMap<String, TipoString> vString = new HashMap<String, TipoString>();
-    HashMap<String, TipoBooleano> vBoolean = new HashMap<String, TipoBooleano>();
-
     for(int i = 0; i < this.conteudoAnalisado.size(); i++) {
       String linha = this.conteudoAnalisado.get(i);
       String[] linhaSplit = linha.split(" ");
       String nome;
+
+      if(linha.endsWith(".") == false || linha.length() < 1) {
+        // System.out.println("assad");
+        continue;
+      }
 
       switch(linhaSplit[0]) {
       	case "summon":
@@ -77,31 +81,59 @@ public class Leitor {
               TipoInteiro intVar = new TipoInteiro();
               nome = linhaSplit[2].replace(".", "");
               intVar.setNome(nome);
-              vInt.put(nome, intVar);
+              this.vInt.put(nome, intVar);
               break;
 
             case "double":
               TipoDouble doubleVar = new TipoDouble();
               nome = linhaSplit[2].replace(".", "");
               doubleVar.setNome(nome);
-              vDouble.put(nome, doubleVar);
+              this.vDouble.put(nome, doubleVar);
               break;
 
             case "string":
               TipoString stringVar = new TipoString();
               nome = linhaSplit[2].replace(".", "");
               stringVar.setNome(nome);
-              vString.put(nome, stringVar);
+              this.vString.put(nome, stringVar);
               break;
 
             case "boolean":
               TipoBooleano booleanVar = new TipoBooleano();
               nome = linhaSplit[2].replace(".", "");
               booleanVar.setNome(nome);
-              vBoolean.put(nome, booleanVar);
+              this.vBoolean.put(nome, booleanVar);
               break;
           }
       	  break;
+
+        case "cast":
+          // System.out.println(linhaSplit[1]);
+
+          if (this.vInt.containsKey(linhaSplit[1])) {
+            int valor = linhaSplit[3].replace(".", "").length();
+            this.vInt.get(linhaSplit[1]).setValor(valor);
+            System.out.println(linhaSplit[1] + " \t: int" + "\t: valor: " + valor);
+          }
+
+          else if (this.vDouble.containsKey(linhaSplit[1])) {
+            int valor = linhaSplit[3].replace(".", "").length();
+            this.vDouble.get(linhaSplit[1]).setValor(valor);
+            System.out.println(linhaSplit[1] + " \t: double" + "\t: valor: " + valor);
+          }
+
+          else if (this.vString.containsKey(linhaSplit[1])) {
+            String valor = linhaSplit[3].replace(".", "");
+            this.vString.get(linhaSplit[1]).setValor(valor);
+            System.out.println(linhaSplit[1] + " \t: string" + "\t: valor: " + valor);
+          }
+
+          else if (this.vBoolean.containsKey(linhaSplit[1])) {
+            String valor = linhaSplit[3].replace(".", "");
+            this.vBoolean.get(linhaSplit[1]).setValor(valor);
+            System.out.println(linhaSplit[1] + " \t: boolean" + "\t: valor: " + valor);
+          }
+
       }
     }
   }
